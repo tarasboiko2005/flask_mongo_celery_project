d4bdca42574bd162a88baa86d34d1b12293f3d13
Taras Boiko <tarasboiko2005@gmail.com>
2026-02-04 15:27:55 +0200

    update create_all

--- app/factory.py
+++ app/factory.py
@@ -135,7 +135,7 @@ def create_app():
     admin.add_view(ModelView(Job, db.session))
 
     with app.app_context():
-        db.create_all(checkfirst=True)
+        db.metadata.create_all(bind=db.engine)
 
     Settings.setup_logging(app)
 
